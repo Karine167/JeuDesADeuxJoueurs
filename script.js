ac3ec71400584fdb46547016b5200c0195f875c1
Karine167 <rude.karine@gmail.com>
2023-11-09 20:03:33 +0100

    ajout des scores tempo et gestion du hold

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const afficheScoreTempo = (idtempo,tempo)=>{
   pTot.textContent = tempo;
 }
 // Définition des fonctions d'affichage des messages
-const afficheIndicatorMessage = (idIndic)=>{
+const afficheIndicatorMessage = (idIndic, message)=>{
   const indicator = document.getElementById(idIndic);
-  indicator.textContent = "You have to play";
+  indicator.textContent = message;
 }
 const effaceIndicatorMessage = (idIndic)=>{
   const indicator = document.getElementById(idIndic);
@@ -32,15 +32,48 @@ class Player{
   afficheTempo(){
     afficheScoreTempo(this.idtempo,this.tempo);
   }
-  afficheMessage(){
-    afficheIndicatorMessage(this.indicator);
+  afficheMessage(message){
+    afficheIndicatorMessage(this.indicator, message);
   }
   effaceMessage(){
     effaceIndicatorMessage(this.indicator);
   }
 }
-
-
+// Création d'une fonction pour gérer un tour de jeu
+const gameTour = (playergame, otherplayer)=>{
+  const rollDice = document.getElementById('rollDice');
+  const hold = document.getElementById('hold');
+    rollDice.addEventListener('click', ()=>{
+      let resultDe = Math.floor(Math.random() * 6)+1;
+      const de = document.getElementById('de');
+      de.textContent = resultDe;
+      if (resultDe == 1) {
+        playergame.tempo = 0;
+        playergame.afficheTempo();
+        playergame.mustPlay = false;
+        otherplayer.mustPlay = true;
+        rollDice.removeEventListener('click');
+        otherplayer.afficheMessage("You have to play");
+        playergame.effaceMessage();
+        gameTour(otherplayer,playergame);
+      }else{
+        playergame.tempo += resultDe;
+        playergame.afficheTempo();
+      }
+    });
+    hold.addEventListener('click', ()=>{
+      playergame.total += playergame.tempo;
+      playergame.tempo = 0;
+      playergame.afficheTempo();
+      playergame.afficheTotal();
+      playergame.mustPlay = false;
+      otherplayer.mustPlay = true;
+      hold.removeEventListener('click');
+      otherplayer.afficheMessage("You have to play");
+      playergame.effaceMessage();
+      gameTour(otherplayer,playergame);
+    });
+} 
 // création d'un écouteur d'événement sur le bouton newgame
 const newGame = document.getElementById('newGame');
 newGame.addEventListener('click', ()=>{
@@ -53,14 +86,22 @@ newGame.addEventListener('click', ()=>{
   player2.afficheTempo();
   //while (player1.total<100 && player2.total<100){
     if (player1.mustPlay){
-      player1.afficheMessage();
+      player1.afficheMessage("You have to play");
       player2.effaceMessage();
+      gameTour(player1,player2);
     }else{
-      player2.afficheMessage();
+      player2.afficheMessage("You have to play");
       player1.effaceMessage();
+      gameTour(player2,player1)
     }
   //}
-  
-})
+  /* if (player1.total>=100){
+    player1.afficheMessage('You Won !! Congratualtions !');
+    player2.afficheMessage('You lose !! Sorry !');
+  }else{
+    player2.afficheMessage('You Won !! Congratualtions !');
+    player1.afficheMessage('You lose !! Sorry !');
+  }  */
+});
 
 
